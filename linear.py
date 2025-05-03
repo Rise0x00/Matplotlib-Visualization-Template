6035e65b8535aadb2a3d81a6f5cb83fcfb8acdc9
Rise0x00 <rise.dhmn@gmail.com>
2025-05-04 02:19:13 +0300

    Add label

--- linear.py
+++ linear.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 x = [1, 2, 3, 4, 5]
 y = [2, 4, 1, 5, 3]
 
-plt.plot(x, y, marker='o', linestyle='--', color='red')
+plt.plot(x, y, marker='o', linestyle='--', color='red', label='Линия')
 plt.title('Пример линейного графика')  # Заголовок
 plt.xlabel('Ось X')  # Подпись оси X
 plt.ylabel('Ось Y')  # Подпись оси Y
